1f86edb29a2a239ac5ea2470ada017828e3782e7
Jonah Snider <me@jonahsnider.ninja>
2018-10-27 11:24:21 -0700

    refactor: Log function arguments

--- src/main/java/org/usfirst/frc/team581/robot/Dashboard.java
+++ src/main/java/org/usfirst/frc/team581/robot/Dashboard.java
@@ -3,8 +3,8 @@ package org.usfirst.frc.team581.robot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Dashboard {
-	public static void log(int key, Object logMe) {
-		SmartDashboard.putString("DB/String " + key, logMe.toString());
+	public static void log(int key, Object logged) {
+		SmartDashboard.putString("DB/String " + key, logged.toString());
 	}
 
 	public static void clear(){
