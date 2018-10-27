b68dcb9c5a312e80eb25c375cd6889b57233752e
Jonah Snider <me@jonahsnider.ninja>
2018-10-27 11:24:34 -0700

    docs: Add some JavaDocs for logging

--- src/main/java/org/usfirst/frc/team581/robot/Dashboard.java
+++ src/main/java/org/usfirst/frc/team581/robot/Dashboard.java
@@ -3,10 +3,17 @@ package org.usfirst.frc.team581.robot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Dashboard {
+	/**
+	 * @param key Index of simple log item to log to
+	 * @param logged Something with a `toString()` function to be logged
+	 */
 	public static void log(int key, Object logged) {
 		SmartDashboard.putString("DB/String " + key, logged.toString());
 	}
 
+	/**
+	 * Clear all log content 
+	 */
 	public static void clear(){
 		for(int i = 0; i < 8; i++){
 			log(i, "");
