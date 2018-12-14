ab5d57e68297de3ee0d582b10afefcb969ae2ef6
Jonah Snider <me@jonahsnider.ninja>
2018-12-13 17:02:15 -0800

    style: Change keyword order

--- src/main/java/org/usfirst/frc/team581/robot/Ports.java
+++ src/main/java/org/usfirst/frc/team581/robot/Ports.java
@@ -16,47 +16,47 @@ public class Ports {
    * static means that the value can be read from the class, like Ports.leftMotor,
    * without instantiating it by calling "new Ports()".
    */
-  final public static int gamepad1 = 0;
-  final public static int gamepad2 = 1;
+  public static final int gamepad1 = 0;
+  public static final int gamepad2 = 1;
 
-  final public static int leftMotor = 1;
-  final public static int rightMotor = 0;
-  final public static boolean leftMotorInverted = false;
-  final public static boolean rightMotorInverted = false;
+  public static final int leftMotor = 1;
+  public static final int rightMotor = 0;
+  public static final boolean leftMotorInverted = false;
+  public static final boolean rightMotorInverted = false;
 
-  final public static int rightEncoderChannelA = 2;
-  final public static int rightEncoderChannelB = 3;
-  final public static boolean rightEncoderReverse = false;
+  public static final int rightEncoderChannelA = 2;
+  public static final int rightEncoderChannelB = 3;
+  public static final boolean rightEncoderReverse = false;
 
-  final public static int leftEncoderChannelA = 0;
-  final public static int leftEncoderChannelB = 1;
-  final public static boolean leftEncoderReverse = true;
+  public static final int leftEncoderChannelA = 0;
+  public static final int leftEncoderChannelB = 1;
+  public static final boolean leftEncoderReverse = true;
 
-  final public static int grabLeftMotor = 3;
-  final public static int grabRightMotor = 4;
-  final public static boolean grabLeftMotorInverted = false;
-  final public static boolean grabRightMotorInverted = true;
+  public static final int grabLeftMotor = 3;
+  public static final int grabRightMotor = 4;
+  public static final boolean grabLeftMotorInverted = false;
+  public static final boolean grabRightMotorInverted = true;
 
-  final public static int solenoid1 = 0;
-  final public static int solenoid2 = 1;
+  public static final int solenoid1 = 0;
+  public static final int solenoid2 = 1;
 
-  final public static int compressor = 0;
+  public static final int compressor = 0;
 
   // TODO: The default ID is 0. It is recommended to not use 0 in case you add
   // more talons.
-  final public static int talon1 = 0;
-  final public static int talon2 = 1;
-  final public static boolean talon1Inverted = false;
-  final public static boolean talon2Inverted = true;
-  final public static boolean talonSensorPhase = false;
+  public static final int talon1 = 0;
+  public static final int talon2 = 1;
+  public static final boolean talon1Inverted = false;
+  public static final boolean talon2Inverted = true;
+  public static final boolean talonSensorPhase = false;
 
   // I want to assign each subsystem its own slot(s) to log into.
   // Expand this as we need more logging.
-  final public static int logArm = 4;
-  final public static int logEncLeft = 0;
-  final public static int logEncRight = 5;
-  final public static int logVirtualMotorDistance = 1;
-  final public static int logVirtualMotorRotation = 6;
-  final public static int logDriveCommand = 2;
+  public static final int logArm = 4;
+  public static final int logEncLeft = 0;
+  public static final int logEncRight = 5;
+  public static final int logVirtualMotorDistance = 1;
+  public static final int logVirtualMotorRotation = 6;
+  public static final int logDriveCommand = 2;
 
 }
